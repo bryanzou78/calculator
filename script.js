c91b18e4b8259dc38c6c3decfbaefbee0e67d1e7
Bryan Zou <175367072+bryanzou78@users.noreply.github.com>
2024-09-16 17:00:57 -0700

    add clear button and fix logic for operate function to account for operator clicks instead of equals clicks

--- script.js
+++ script.js
@@ -2,74 +2,101 @@
 const display = document.querySelector('.display');
 const number = document.querySelectorAll('.number-btn');
 const operator = document.querySelectorAll('.operator-btn');
-const clear = document.querySelector('.clear-btn');
 const equals = document.querySelector('.equals-btn');
+const clear = document.querySelector('.clear-btn');
+const deleteBtn = document.querySelector('.delete-btn');
 
 let firstNumber = '';
-let chosenOperator = null;
 let secondNumber = '';
+let currentOperator = null;
+let newOperator = null;
 
 //Add function
 function add (num1, num2) {
-    return num1 + num2;
+    return parseFloat(num1) + parseFloat(num2);
 }
 
 //Subtract function
 function subtract (num1, num2) {
-    return num1 - num2;
+    return parseFloat(num1) - parseFloat(num2);
 }
 
 //Multiply function
 function multiply (num1, num2) {
-    return num1 * num2;
+    return parseFloat(num1) * parseFloat(num2);
 }
 
 //Divide function
 function divide (num1, num2) {
-    return num1 / num2;
+    return parseFloat(num1) / parseFloat(num2);
 }
 
 //Operate function
 function operate () {
     let result;
-    const opnum1 = parseFloat(firstNumber);
-    const opnum2 = parseFloat(secondNumber);
 
-    if (chosenOperator === '+') {
-        result = add(opnum1, opnum2);
-    } else if (chosenOperator === '-') {
-        result = subtract(opnum1, opnum2);
-    } else if (chosenOperator === '*') {
-        result = multiply(opnum1, opnum2);
-    } else if (chosenOperator === '/') {
-        result = divide(opnum1, opnum2);
+    if (currentOperator === '+') {
+        result = add(firstNumber, secondNumber);
+    } else if (currentOperator === '-') {
+        result = subtract(firstNumber, secondNumber);
+    } else if (currentOperator === '*') {
+        result = multiply(firstNumber, secondNumber);
+    } else if (currentOperator === '/') {
+        result = divide(firstNumber, secondNumber);
     }
-
+    console.log(result);
     display.textContent = result;
+    firstNumber = result;
+    secondNumber = '';
+
+    if (currentOperator !== null) {
+        display.textContent = `${firstNumber} ${currentOperator}`;
+    }
 }
 
 //Event listeners
 number.forEach(number => {
     number.addEventListener('click', () => {
-        if (chosenOperator === null) {
+        if (currentOperator === null) {
             firstNumber += number.textContent;
             display.textContent = firstNumber;
         } else {
             secondNumber += number.textContent;
-            display.textContent = `${firstNumber} ${chosenOperator} ${secondNumber}`;
+            display.textContent = `${firstNumber} ${currentOperator} ${secondNumber}`;
         }
     })
 })
 
-
 operator.forEach(operator => {
     operator.addEventListener('click', () => {
-        chosenOperator = operator.textContent;
-        display.textContent = `${firstNumber} ${chosenOperator}`;
+        if (currentOperator === null) {
+            currentOperator = operator.textContent;
+            display.textContent = `${firstNumber} ${currentOperator}`;
+        } else if (currentOperator !== null) {
+            newOperator = operator.textContent;
+            if (currentOperator !== null && firstNumber !== '' && secondNumber !== '') {
+                operate();
+            }
+            currentOperator = newOperator;
+            display.textContent = `${firstNumber} ${currentOperator}`;
+        }
     })
 })
 
 equals.addEventListener('click', () => {
     operate();
+    currentOperator = null;
+    display.textContent = `${firstNumber}`
+})
+
+clear.addEventListener('click', () => {
+    firstNumber = '';
+    currentOperator = null;
+    secondNumber = '';
+    display.textContent = 0;
+})
+
+deleteBtn.addEventListener('click', () => {
+
 })
 
